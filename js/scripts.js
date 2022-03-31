373bbe959473b0270f67ef581c1eb2f6362e1ff2
IosebOne <45330280+iosebi2899@users.noreply.github.com>
2022-03-31 15:07:21 +0400

    chore: added logos & sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,57 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 4,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    720: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-  },
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
+var mySwiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+  breakpoints: {
+    1200: {
+      slidesPerView: 5,
+      loopedSlides: 4,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 5,
+      loopedSlides: 3,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 5,
+      loopedSlides: 2,
+      spaceBetween: 10,
+    },
+    675: {
+      autoplay: true,
+      slidesPerView: 4,
+      loopedSlides: 1,
+      spaceBetween: 20,
+    },
+    250: {
+      autoplay: true,
+      slidesPerView: 2,
+      loopedSlides: 1,
+      spaceBetween: 10,
+    },
+  },
+  on: {
+    init: function () {
+      checkArrow();
+    },
+    resize: function () {
+      checkArrow();
+    },
   },
 });
+
+function checkArrow() {
+  var swiperPrev = document.querySelector(".swiper-button-prev");
+  var swiperNext = document.querySelector(".swiper-button-next");
+  if (window.innerWidth < 768) {
+    console.log("Success", window.innerWidth);
+    swiperPrev.style.display = "block";
+    swiperNext.style.display = "block";
+  } else {
+    swiperPrev.style.display = "none";
+    swiperNext.style.display = "none";
+  }
+}
